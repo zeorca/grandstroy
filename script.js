505faf9cc50845d8161906f1c9346414ec378478
zrc <wierto08@gmail.com>
2023-01-25 00:04:25 +0800

    final

--- script.js
+++ script.js
@@ -69,3 +69,18 @@ document.getElementById("req").addEventListener("submit", function (e) {
       console.log("fin");
     });
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
